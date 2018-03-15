44320038f2f83771b8ed9b4da7616f0e6e58f229
Leitino <alessandro.leita89@gmail.com>
2018-03-15 15:58:19 +0100

    funzione che conta i caratteri di una stringa

--- es2.js
+++ es2.js
@@ -1,3 +1,11 @@
-var Lista = ['casa', 'ciao', 'penna', 'mucca'];
+var Lista = ['casa', 'ciao', 'penna', 'mucca', 'ambarabacciccicocco', 'samurai'];
 var casuale = Lista[Math.floor(Math.random()*Lista.length)];
 console.log(casuale)
+
+function contatore() {
+  x=casuale.length
+  alert(x)
+  return(alert) ;
+
+}
+contatore()
