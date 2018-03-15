3708b75866052c1a0e33240b3c7317c2cfa75dcc
Leitino <alessandro.leita89@gmail.com>
2018-03-15 16:58:47 +0100

    aggiunta di commenti

--- es2.js
+++ es2.js
@@ -1,16 +1,16 @@
-var Lista = ['ciao', 'penna', 'mucca', 'ambarabacciccicocco', 'samurai', 'armadio'];
-var casuale = Lista[Math.floor(Math.random()*Lista.length)];
+var Lista = ['ciao', 'penna', 'mucca', 'ambarabacciccicocco', 'samurai', 'armadio'];//creata la array (lista)
+var casuale = Lista[Math.floor(Math.random()*Lista.length)];//prende un elemento casuale della lista.
 alert(casuale)
 
 function contatore() {
-  x=casuale.length
+  x=casuale.length//conta quanti caratteri ha la parola casuale selezionata
   alert('la parola è composta da ' + x + ' lettere')
   return(alert) ;
 
 }
-contatore()
+contatore()//richiamo della funzione
 
-for (var i = 0; i < 7; i++) {
+for (var i = 0; i < 7; i++) {//inizio del ciclo for
   a=prompt('indovina la parola')
   if (a==casuale) {
     alert('hai indovinato')
